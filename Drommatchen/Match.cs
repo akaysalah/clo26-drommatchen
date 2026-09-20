bc17ab8e89a3d2a24b9ba523b49d99edd67f3b7c
akaysalah <ad07686@skola.goteborg.se>
2026-09-20 11:50:03 +0200

    la till match med fält, properties 6 konstruktor

--- Drommatchen/Match.cs
+++ Drommatchen/Match.cs
@@ -8,25 +8,30 @@ public class Match
     // Properties — publik get, privat set
     public string Hemmalag
     {
-        // get och privat set för Hemmalag 
+        get { return _hemmalag; }
+        private set { _hemmalag = value; }
     }
 
     public string Bortalag
     {
-        // get och privat set för Bortalag
+        get { return _bortalag; }
+        private set { _bortalag = value; }
     }
 
     public string Datum
     {
-        // get och privat set för Datum
+        get { return _datum; }
+        private set { _datum = value; }
     }
 
-    // Pluspoäng om ni kommer på hur man kan minska ner koden med properties :)
+    // Pluspoäng om ni kommer på hur man kan minska ner koden med properties :) <- ja, genom auto-properties men jag det tog den långa vägen istället för att lära mig. I framtiden använder jag nog auto-properties istället.
 
     // Konstruktor
     public Match(string hemmalag, string bortalag, string datum)
     {
-        // TODO: tilldela de privata fälten
+        _hemmalag = hemmalag;
+        _bortalag = bortalag;
+        _datum = datum;
     }
 
     // Metod 1: skriv ut matchens lag och datum
